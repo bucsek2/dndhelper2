638ce3f5d557b2d5f76053f414050c31e14e9be9
György Bucsek <gyorgy.bucsek@gmail.com>
2019-08-12 11:14:19 +0200

    Add progress

---
+++ src/Character/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Panel} from 'primereact/panel';
+
+export class Character extends React.Component {
+    render() {
+        return (
+            <Panel header="player name1">
+                lol wut?
+            </Panel>
+        );
+    }
+}
