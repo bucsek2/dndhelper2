5388ffee5c23744675fba8e1629eda40f7939c94
György Bucsek <gyorgy.bucsek@gmail.com>
2019-08-13 15:27:26 +0200

    Beautify survival helper

--- src/component/character/Character.tsx
+++ src/component/character/Character.tsx
@@ -3,7 +3,7 @@ import { Panel } from 'primereact/panel';
 import { Checkbox } from 'primereact/checkbox';
 import { Spinner } from 'primereact/spinner';
 import { Button } from 'primereact/button';
-import "./Character.css";
+import "./Character.scss";
 
 export interface CharacterProps extends CharacterModel {
     onChange: (state: CharacterModel)=>void
@@ -62,22 +62,24 @@ export class Character extends React.Component<CharacterProps, {}> {
     }
 
     render() {
-        console.log("render "+this.props.name);
-        
-        let forage: JSX.Element[] = []
-
-        forage.push(<span>Survival</span>)
-        forage.push(<Spinner value={this.props.survival} onChange={e=> this.updateSurvival(e.value)} formatInput={true} decimalSeparator={','} thousandSeparator={' '} min={-30} max={30}/>)
-        forage.push(<span>Wisdom</span>)
-        forage.push(<Spinner value={this.props.wisdom} onChange={e=> this.updateWisdom(e.value)} formatInput={true} decimalSeparator={','} thousandSeparator={' '} min={-30} max={30}/>)
+        const forage = <div className="forage-panel">
+            <div className="survival">
+                <div className="survival-label">Survival</div>
+                <Spinner value={this.props.survival} onChange={e=> this.updateSurvival(e.value)} formatInput={true} decimalSeparator={','} thousandSeparator={' '} min={-30} max={30}/>
+            </div>
+            <div className="wisdom">
+                <div className="wisdom-label">Wisdom</div>
+                <Spinner value={this.props.wisdom} onChange={e=> this.updateWisdom(e.value)} formatInput={true} decimalSeparator={','} thousandSeparator={' '} min={-30} max={30}/>
+            </div>
+        </div>;
 
         return (
             <Panel header={this.props.name} className="character">
+                <Button className="remove-button" label="X" onClick={e=> this.props.onRemove(this.props.id)}/>
                 <div className="forage">
                     <span className="forage-label">Forage</span>
                     <Checkbox className="forage-box" onChange={e=> this.updateForage(e.checked)} checked={this.props.forage}/>
                 </div>
-                <Button className="remove-button" label="X" onClick={e=> this.props.onRemove(this.props.id)}/>
                 {this.props.forage &&
                     forage
                 }           
